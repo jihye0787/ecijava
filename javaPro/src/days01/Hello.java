c7382026cb418793d938179ee781ecf5bc15cab1
jihye0787 <79951175+jihye0787@users.noreply.github.com>
2021-03-03 12:11:46 +0900

    Update Hello.java
    Hong Hello.java Coding Edit

--- javaPro/src/days01/Hello.java
+++ javaPro/src/days01/Hello.java
@@ -3,6 +3,9 @@ package days01;
 public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		
+		//홍길동 팀원이 코딩 추가/수정
+		System.out.println("hong");
 	}
 
 }
